c59cfa35d060038900c48d5d9bdde7543c2b8883
Xuan Phuc Tran <xuanphuctran269@gmail.com>
2025-03-14 23:19:00 +0700

    Add user DTOs and validation for user creation and updates

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEnum, IsEmail, IsString, IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsEnum(["INTERN", "ENGINEER", "ADMIN"], {
+        message: 'Valid role required'
+    })
+    role: "INTERN" | "ENGINEER" | "ADMIN";
+}
